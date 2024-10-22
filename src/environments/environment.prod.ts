d7fe723d24db7ed1c947ff4c03e424e0ef51a1e9
John Satur <animashorse3@yahoo.com.mx>
2024-10-21 22:04:32 -0600

    Endpoint corregido

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environments = {
-    baseAPIUrl: 'https://lively-cuddle-578168b048.strapiapp.com//api',
+    baseAPIUrl: 'https://lively-cuddle-578168b048.strapiapp.com/api',
     baseUrl: 'https://lively-cuddle-578168b048.strapiapp.com/',
     baseMediaUrl: '',
 };
