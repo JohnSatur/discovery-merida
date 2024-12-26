48be4b32a5a909b26995961bd7168e3a2f14d850
John Satur <animashorse3@yahoo.com.mx>
2024-12-26 16:55:56 -0600

    Actualización de las preguntas frecuentes

--- src/app/shared/components/faq-section/faq-section.component.ts
+++ src/app/shared/components/faq-section/faq-section.component.ts
@@ -10,10 +10,10 @@ import { FaqItemComponent } from '@components/faq-item/faq-item.component';
 })
 export class FaqSectionComponent {
   public faqs = [
-    { question: '¿Cómo puedo hacer una reserva?', answer: '...' },
-    { question: '¿Cuál es la política de cancelación?', answer: '...' },
-    { question: '¿Cuáles son las formas de pago aceptadas?', answer: '...' },
-    { question: '¿Cómo puedo contactar al anfitrión?', answer: '...' },
-    { question: '¿Qué pasa si tengo algún problema durante mi estancia?', answer: '...' },
+    { question: '¿Cómo puedo hacer una reserva?', answer: 'Solo haz click en la casa de tu preferencia y te llevará a nuestro vínculo de Airbnb para que consultes disponibilidad y precio.' },
+    { question: '¿Cuál es la política de cancelación?', answer: 'Nuestra política de cancelación es moderada, por lo que puedes cancelar sin penalización hasta 5 días antes de tu llegada.' },
+    { question: '¿Cuáles son las formas de pago aceptadas?', answer: 'Las formas de pago son tarjeta de crédito en la plataforma de Airbnb.' },
+    // { question: '¿Cómo puedo contactar al anfitrión?', answer: '...' },
+    // { question: '¿Qué pasa si tengo algún problema durante mi estancia?', answer: '...' },
   ];
 }
